0f1cb2e2d0dc1520eeb1715fdc0f7517126d834a
tahaali73 <121288774+tahaali73@users.noreply.github.com>
2024-11-18 07:26:37 -0800

    sports_form_on sqlite3

--- sports.py
+++ sports.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
 SPORTS=["circket", "football", "basketball"]
-PARTICIPANTS={}
 
 @app.route("/")
 def form():
@@ -12,17 +18,35 @@ def form():
 @app.route("/success", methods=["POST"])
 def success():
     name = request.form.get("name")
-    if not name in PARTICIPANTS:
+    if not name:
         return render_template("failure.html")
     sports = request.form.get("sport")
     if sports not in SPORTS:
         return render_template("/failure.html")
-    PARTICIPANTS[name] = sports
+    
+    conn = get_db_connection()
+    conn.execute("insert into games(name,sports) values(?, ?)",(name, sports))
+    conn.commit()
+    conn.close()
+
     return render_template("formConfirm.html")
 
 @app.route("/registrants")
 def reg():
-    return render_template("registrants.html", registrants=PARTICIPANTS)
+    conn = get_db_connection()
+    participants = conn.execute("select * from games").fetchall()
+    conn.close()
+    return render_template("registrants.html", registrant=participants)
+
+@app.route("/deregister", methods=["POST"] )
+def deregistrant():
+    id = request.form.get("id")
+    conn = get_db_connection()
+    conn.execute("DELETE FROM games WHERE id = ?", (id,))
+    conn.commit()
+    conn.close()
+    return redirect("/registrants")
+
 
 if __name__ == "__main__":
     app.run()
